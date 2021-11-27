5e7cc60af4e2e7e845e5368df71b86310bf0149a
Artur SD <arko8919@gmail.com>
2021-11-27 13:56:05 +0000

    reversing changes

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+// References to DOM elements
+const buttonRef = document.querySelectorAll('button[type="button"]');
+const inputRef = document.querySelector('#input');
+const sspanErrorRef = document.querySelector('#input + span.error');
+const outputRef = document.querySelector('#output');
+console.log(sspanErrorRef);
+
+export { sspanErrorRef };
