32153aecfdc6075ebe66d908caef84615ddba81b
Artur SD <arko8919@gmail.com>
2021-11-27 12:40:22 +0000

    Initial dist subtree commit

--- src/js/copy-to-clipboard.js
+++
@@ -1,7 +0,0 @@
-const outputRef = document.querySelector('#output');
-
-export function copyToClipboard() {
-    outputRef.select();
-    outputRef.setSelectionRange(0, 99999); /* For mobile devices */
-    document.execCommand("copy");
-}
--- src/js/show-api-error.js
+++
@@ -1,5 +0,0 @@
-export function showApiError(spanErrorRef, error) {
-    spanErrorRef.textContent = 'Network error when attempting to fetch resource.';
-    spanErrorRef.className = 'error active';
-    console.log(error);
-}
