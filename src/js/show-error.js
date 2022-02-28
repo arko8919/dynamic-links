6a5c7bd85717433b0193151473dad96bdd1b6183
Artur SD <arko8919@gmail.com>
2022-02-28 12:35:11 +0000

    typing errors fixed

--- src/js/show-error.js
+++ src/js/show-error.js
@@ -3,12 +3,12 @@ import { checkPattern } from './check-pattern';
 export function showError(inputRef, divErrorRef) {
     if (inputRef.validity.valueMissing) {
         // If the field is empty, display the following error message
-        divErrorRef.textContent = 'You need to enter an url address.';
+        divErrorRef.textContent = 'You need to enter an URL address.';
     } else if (!checkPattern()) {
-        divErrorRef.textContent = 'Entered value needs to be an e-mail address.';
+        divErrorRef.textContent = 'Entered value needs to be an URL address.';
     } else if (inputRef.validity.typeMismatch) {
         // If the field doesn't contain an email address, display the following error message
-        divErrorRef.textContent = 'Entered value needs to be an e-mail address.';
+        divErrorRef.textContent = 'Entered value needs to be an URL address.';
     }
 
     divErrorRef.className = 'error active';
